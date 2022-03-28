38bb3a6d804220bf6a07c927ce552b4f21723b08
ECHOSHIH <chunyushih@outlook.com>
2022-03-28 12:01:44 +0800

    [doc] remove excess

--- src/pages/ToDoList/index.js
+++ src/pages/ToDoList/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import AddItemInput from "./components/AddItemInput";
 import FilterBar from "./components/FilterBar";
 import ListItems from "./components/ListItems";
